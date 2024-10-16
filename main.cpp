0c52d40b9a641077edb99e6d3d2fede9a5b2eea0
POURRESY Benjamin <Benjamin.Pourresy@bordeaux-inp.fr>
2024-10-16 10:49:12 +0200

    Clignotement de la led à une fréquence de 1hz

--- main.cpp
+++ main.cpp
@@ -1,38 +1,51 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2019 ARM Limited
- * SPDX-License-Identifier: Apache-2.0
- */
 #include <ostream>
 #include "mbed.h"
 #include "PinNames.h"
 
-// Blinking rate in milliseconds
 #define BLINKING_RATE 500ms
+
+DigitalOut led(LED1);
+InterruptIn bt(BUTTON1);
+Timer button_timer;
+Ticker led_freq;
+int temps;
+// Calcul du temps d'appuis
+/*
+void start_timer()
+{
+    button_timer.start();  // Démarre le chronomètre
+}
+
+
+void stop_timer()
+{
+    button_timer.stop();
+    temps = button_timer.elapsed_time().count();
+    button_timer.reset();
+}
+ */
+
+void flip()
+{
+    led = !led;
+}
 
 int main()
 {
-    // Initialise the digital pin LED1 as an output
-//#ifdef LED1
-    DigitalOut led(LED1);
-//#else
-  //  bool led;
-//#endif
 
-//#ifdef SW1
-    DigitalInOut bt(BUTTON1);
+    led_freq.attach(&flip, 500ms); // LED clignote à une fréquence de 1 Hz (500ms on, 500ms off)
 
-//#else
-//    bool sw;
-//#endif
+    // bt.rise(&flip);
+    // bt.rise(&start_timer);  // Appui du bouton
+    // bt.fall(&stop_timer);   // Relâchement du bouton
 
     while (true)
     {
 
-       led = bt.read();
-
-
+        // led = bt.read();
+        // printf("Durée de l'appui: %d ms\n", temps / 1000);
         // led = !led;
-        ThisThread::sleep_for(BLINKING_RATE);
-        //printf("Hello World!\n");
+        // ThisThread::sleep_for(BLINKING_RATE);
+        //  printf("Hello World!\n");
     }
 }
